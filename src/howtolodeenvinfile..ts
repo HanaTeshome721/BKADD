c4bca283f278f40a8bb0bf4a1e35467f976cff3a
HanaTeshome721 <hana2729tm@gmail.com>
2025-11-30 19:49:19 +0300

    fix :add md

--- src/howtolodeenvinfile..ts
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import dotenv from 'dotenv';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_CONNECTION_STRING = exports.JWT_SECRET = void 0;
-// Load environment variables from .env file
-// dotenv.config();
-exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
